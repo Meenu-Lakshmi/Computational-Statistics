c7f3bbb7f4025a78290ef39429476e87c9cfd491
Meenu Lakshmi <meenulakshmi1617@gmail.com>
2024-10-17 14:43:06 +0530

    Create avgsumvar.py

---
+++ avgsumvar.py
@@ -0,0 +1,9 @@
+import numpy as np
+A = np.array([1,2,3,4,5])
+average = np.mean(A)
+variance = np.var(A)
+sd = np.std(A)
+
+print("Average:",average)
+print("Variance:",variance)
+print("Standard Deviation:",sd)
